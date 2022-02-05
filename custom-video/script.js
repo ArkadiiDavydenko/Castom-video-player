dd1fc10be0d785f4996be22330bf9b905ea46eee
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-02-05 17:10:41 +0300

    feat: add progress line

--- custom-video/script.js
+++ custom-video/script.js
@@ -4,46 +4,77 @@ const sliderVolume = player.querySelector('.volume-slider');
 const video = player.querySelector('.viewer');
 const toggleVideo = player.querySelector('.toggle-video');
 const toggleVolume = player.querySelector('.toggle-volume')
-
-console.log(sliderVolume.value)
-video.volume = (sliderVolume.value/100);
-
+const playVideo  = player.querySelector('.main-button')
+const progressVideo = player.querySelector('.progress__line');
+const progress = player.querySelector('.progress')
+const volume = sliderVolume.value/100;
+video.volume = volume;
+let progression;
 
 function switchPlay() {
     if (video.paused) {
-        video.play()
+        video.play();
+        playVideo.style.display = 'none';
         toggleVideo.style.backgroundImage = 'url("./assets/svg/pause.svg")';
+        progression = window.setInterval(handleProgress, 10);
     } else {
-        video.pause()
+        video.pause();
+        playVideo.style.display = 'block';
         toggleVideo.style.backgroundImage = 'url("./assets/svg/play.svg")';
     }
 }
 
 function switchVolume() {
-    console.log('push')
-    if (video.volume) {
-        // console.log(video.volume)
+    if (sliderVolume.value == 0) {
+        video.volume = 0.4;
+        sliderVolume.value = video.volume*100;
+        toggleVolume.style.backgroundImage = 'url("./assets/svg/volume.svg")';
+        changeInput ();
+    }
+    else if (video.volume) {
         video.volume = 0;
         toggleVolume.style.backgroundImage = 'url("./assets/svg/mute.svg")';
+        sliderVolume.value = 0;
+        changeInput ();
     } else {
-        // console.log(video.volume)
-        video.volume = (sliderVolume.value/100);
+        video.volume = sliderVolume.value/100;
         toggleVolume.style.backgroundImage = 'url("./assets/svg/volume.svg")';
     }
 }
 
-
-function changeIcon(event) {
+function changeInput () {
+    video.volume = (sliderVolume.value/100)
+    const value = sliderVolume.value;
+    if (sliderVolume.value == 0) {
+        console.log(sliderVolume.value )
+        toggleVolume.style.backgroundImage = 'url("./assets/svg/mute.svg")';
+        sliderVolume.style.background = `linear-gradient(to right, var(--main-gold) 0%, var(--main-gold) ${value}%, #fff ${value}%, white 100%)`
+    }
+    else {
+        toggleVolume.style.backgroundImage = 'url("./assets/svg/volume.svg")';
+        sliderVolume.style.background = `linear-gradient(to right, var(--main-gold) 0%, var(--main-gold) ${value}%, #fff ${value}%, white 100%)`
+    }
 
 }
 
-video.addEventListener('click', switchPlay)
-toggleVideo.addEventListener('click', switchPlay)
-toggleVolume.addEventListener('click', switchVolume)
+function handleProgress() {
+    const percent = (video.currentTime/video.duration)*100;
+    progressVideo.style.flexBasis = `${percent}%`;
 
-sliderVolume.addEventListener('input', function () {
-    video.volume = (this.value/100)
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, var(--main-gold) 0%, var(--main-gold) ${value}%, #fff ${value}%, white 100%)`
-})
+}
+function scrub (event) {
+    const scrubTime = (event.offsetX/progress.offsetWidth)*video.duration;
+    video.currentTime = scrubTime;
+    handleProgress();
+
+}
+
+video.addEventListener('click', e => !video.paused && switchPlay(e));
+video.addEventListener('ended', handleProgress);
+video.addEventListener('change', handleProgress);
+toggleVideo.addEventListener('click', switchPlay);
+playVideo.addEventListener('click', switchPlay);
+toggleVolume.addEventListener('click', switchVolume);
+sliderVolume.addEventListener('input',changeInput );
+progress.addEventListener('click', scrub)
 
