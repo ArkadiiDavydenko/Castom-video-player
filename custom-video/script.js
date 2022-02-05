ef6d1c9a8cab635c97a01805ca5d4a6a2ba909a4
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-02-05 19:41:57 +0300

    feat: add fullScreen

--- custom-video/script.js
+++ custom-video/script.js
@@ -3,14 +3,19 @@ const player = document.querySelector('.player');
 const sliderVolume = player.querySelector('.volume-slider');
 const video = player.querySelector('.viewer');
 const toggleVideo = player.querySelector('.toggle-video');
-const toggleVolume = player.querySelector('.toggle-volume')
-const playVideo  = player.querySelector('.main-button')
+const toggleVolume = player.querySelector('.toggle-volume');
+const playVideo = player.querySelector('.main-button');
 const progressVideo = player.querySelector('.progress__line');
-const progress = player.querySelector('.progress')
-const volume = sliderVolume.value/100;
+const progress = player.querySelector('.progress');
+const controlFullScreen = player.querySelector('.player-fullscreen');
+// const slider = player.querySelector('.slider');
+const volume = sliderVolume.value / 100;
 video.volume = volume;
 let progression;
+let mousedown;
 
+
+console.log(progress.offsetWidth)
 function switchPlay() {
     if (video.paused) {
         video.play();
@@ -27,30 +32,27 @@ function switchPlay() {
 function switchVolume() {
     if (sliderVolume.value == 0) {
         video.volume = 0.4;
-        sliderVolume.value = video.volume*100;
+        sliderVolume.value = video.volume * 100;
         toggleVolume.style.backgroundImage = 'url("./assets/svg/volume.svg")';
-        changeInput ();
-    }
-    else if (video.volume) {
+        changeInput();
+    } else if (video.volume) {
         video.volume = 0;
         toggleVolume.style.backgroundImage = 'url("./assets/svg/mute.svg")';
         sliderVolume.value = 0;
-        changeInput ();
+        changeInput();
     } else {
-        video.volume = sliderVolume.value/100;
+        video.volume = sliderVolume.value / 100;
         toggleVolume.style.backgroundImage = 'url("./assets/svg/volume.svg")';
     }
 }
 
-function changeInput () {
-    video.volume = (sliderVolume.value/100)
+function changeInput() {
+    video.volume = (sliderVolume.value / 100)
     const value = sliderVolume.value;
     if (sliderVolume.value == 0) {
-        console.log(sliderVolume.value )
         toggleVolume.style.backgroundImage = 'url("./assets/svg/mute.svg")';
         sliderVolume.style.background = `linear-gradient(to right, var(--main-gold) 0%, var(--main-gold) ${value}%, #fff ${value}%, white 100%)`
-    }
-    else {
+    } else {
         toggleVolume.style.backgroundImage = 'url("./assets/svg/volume.svg")';
         sliderVolume.style.background = `linear-gradient(to right, var(--main-gold) 0%, var(--main-gold) ${value}%, #fff ${value}%, white 100%)`
     }
@@ -58,23 +60,41 @@ function changeInput () {
 }
 
 function handleProgress() {
-    const percent = (video.currentTime/video.duration)*100;
+    const percent = (video.currentTime / video.duration) * 100;
     progressVideo.style.flexBasis = `${percent}%`;
 
+    // slider.style.left = `${percent}%`;
 }
-function scrub (event) {
-    const scrubTime = (event.offsetX/progress.offsetWidth)*video.duration;
+
+function scrub(event) {
+    const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
     handleProgress();
 
 }
 
+function reloadVideo() {
+    video.currentTime = 0;
+    playVideo.style.display = 'block';
+    toggleVideo.style.backgroundImage = 'url("./assets/svg/play.svg")';
+
+}
+
+function goFullScreen(){
+    console.dir('work');
+    if(video.webkitSupportsFullscreen) video.webkitEnterFullScreen();
+}
+
 video.addEventListener('click', e => !video.paused && switchPlay(e));
-video.addEventListener('ended', handleProgress);
+video.addEventListener('ended', reloadVideo);
 video.addEventListener('change', handleProgress);
 toggleVideo.addEventListener('click', switchPlay);
 playVideo.addEventListener('click', switchPlay);
 toggleVolume.addEventListener('click', switchVolume);
-sliderVolume.addEventListener('input',changeInput );
+sliderVolume.addEventListener('input', changeInput);
+controlFullScreen.addEventListener('click', goFullScreen);
 progress.addEventListener('click', scrub)
+progress.addEventListener('mousedown', () => mousedown = true, console.log('true'));
+document.addEventListener('mouseup', () => mousedown = false, console.log('false'));
+progress.addEventListener('mousemove', (event) => mousedown && scrub(event));
 
